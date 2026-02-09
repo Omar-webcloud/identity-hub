4cce1bccd498aa07b37ffd6e7bc2d0fc0ebe85eb
Md Omar Faruk Chowdhury <ofcjim16@gmail.com>
2026-02-09 21:13:28 +0600

    swiper js pagination

---
+++ script.js
@@ -0,0 +1,33 @@
+ 
+ 
+ 
+ 
+ const swiper = new Swiper("#testimonialsSwiper", {
+        loop: true,
+        autoplay: {
+          delay: 1000000, 
+          disableOnInteraction: false,
+        },
+        pagination: {
+          el: "#dotsContainer",
+          clickable: true,
+          renderBullet: (index, className) => {
+            return `<button class="dot ${className}" data-index="${index}"></button>`;
+          },
+        },
+        navigation: {
+          nextEl: ".btn-next",
+          prevEl: ".btn-prev",
+        },
+        speed: 600,
+        effect: "slide",
+      });
+
+      
+      swiper.on("slideChange", () => {
+        document.querySelectorAll(".dot").forEach((dot, index) => {
+          dot.classList.toggle("active", index === swiper.realIndex);
+        });
+      });
+     
+      document.querySelectorAll(".dot")[0].classList.add("active");
