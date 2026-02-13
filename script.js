0d6485761befb16da479d4ab74cc20f80d6a12f3
Md Omar Faruk Chowdhury <ofcjim16@gmail.com>
2026-02-13 22:24:01 +0600

    add nav-toggle

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
- 
+ const toggle = document.getElementById("navbarToggle");
+const menu = document.getElementById("navbarMenu");
+
+toggle.addEventListener("click", () => {
+  const isOpen = menu.classList.toggle("is-open");
+  toggle.setAttribute("aria-expanded", isOpen);
+});
+
  
  
  
